dcea7dda4c766019dd32d641a4bcb408330dbbc6
Juan Pablo Ante <juan.pablo.ante@correounivalle.edu.co>
2024-11-08 00:59:03 -0500

    Todos los permisos corregidos

--- Backend/users/permissions.py
+++ Backend/users/permissions.py
@@ -3,29 +3,11 @@ from .models import Usuario, Rol
 from functools import wraps
 from django.http import JsonResponse
 
-class IsAdmin(BasePermission):
+from rest_framework import permissions
+
+class IsStaffUser(permissions.BasePermission):
+    """
+    Permiso personalizado que solo permite acceso a usuarios staff.
+    """
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.rol == 'admin'
-
-class IsVendedor(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.rol == 'vendedor'
-
-class IsComprador(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.rol == 'comprador'
-
-
-def admin_required(view_func):
-    @wraps(view_func)
-    def _wrapped_view(request, *args, **kwargs):
-        user = request.user
-        if not user.is_authenticated:
-            return JsonResponse({'message': 'No autenticado'}, status=401)
-        
-        # Verificar si el usuario tiene el rol de "Admin"
-        if user.rol.nombre != "Admin":
-            return JsonResponse({'message': 'Permiso denegado'}, status=403)
-        
-        return view_func(request, *args, **kwargs)
-    return _wrapped_view
+        return bool(request.user and request.user.is_authenticated and request.user.is_staff)
