60437df0b83d65ff05794e058050148ead01a8db
Jpdv9 <jean.davalos@correounivalle.edu.co>
2024-12-11 21:10:04 -0500

    Rescatando el package.json, GRACIAS MIGUEL

--- Backend/ecommerce_project/urls.py
+++ Backend/ecommerce_project/urls.py
@@ -54,5 +54,4 @@ urlpatterns = [
     path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/stripe/', include('stripe_payments.urls')),
 
-]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
